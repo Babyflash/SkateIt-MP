132aaa089c8728db3f1ddc1280691a9911397748
Sevan Golnazarian <sgolnaza@gmail.com>
2019-04-27 17:13:14 +0800

    app.js includes login request

--- app.js
+++ app.js
@@ -1,39 +1,123 @@
-//app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // WX code
+    let app = this;
+    let salmon2 = "mo2"
 
-    // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success: function (res) {
+        console.log("RES from wechat:")
+        console.log(res)
+        if (res.code) {
+          // console.log("wanna eat salmon")
+          app.getUserInfo((userInfoFromCallBackHell) => {
+            // console.log("Print userinfo callback hell:")
+            // console.log(userInfoFromCallBackHell)
+            wx.request({
+              success: function (res) {
+                try {
+                  console.log("Result from server: ")
+                  console.log(res)
+
+                  wx.setStorageSync('token', res.data.authentication_token)
+                  wx.setStorageSync('currentUserId', res.data.id)
+
+                  app.globalData.token = res.data.authentication_token
+                  app.globalData.currentUserId = res.data.id
+                  app.globalData.email = res.data.email
+                  app.globalData.user = res.data
+                  // console.log("TEST Res store globalData >>>")
+                  // console.log(app.globalData.token)
+                  // console.log(app.globalData.currentUserId)
+                  // console.log(app.globalData.email)
+                  // console.log(app.globalData.user)
+                } catch (e) {
+                  console.log("Didn't set storage")
+                }
+              },
+
+              url: 'http://localhost:3000' + '/api/v1/users',
+              method: "post",
+              header: {
+                'content-type': 'application/json'
+              },
+              data: {
+                code: res.code,
+                user: {
+                  name: userInfoFromCallBackHell.nickName,
+                  avatar_url: userInfoFromCallBackHell.avatarUrl,
+                  gender: userInfoFromCallBackHell.gender,
+                  province: userInfoFromCallBackHell.province,
+                  city: userInfoFromCallBackHell.city
+                }
+              }
+            })
+
+          }
+            , app)
+
+          //发起网络请求
+          // console.log("Yes..We got code from RES")
+          // console.log("Global Data before RES")
+
+        } else {
+          console.log('error' + res.errMsg)
+        }
+
+        // ## Send request card to avoid asyc
+        // wx.request({
+        //   success: function (res) {
+        //     try {
+        //       console.log("MMMINDEX API: ")
+        //       // console.log(res)
+        //       app.globalData.items = res.data.items
+        //       console.log(app.globalData.items)
+        //       console.log("INDEX API SUCCESS")
+        //     } catch (e) {
+        //       console.log(e)
+        //     }
+        //   },
+
+        //   url: 'https://jinma.herokuapp.com/api/v1/items',
+        //   method: "get"
+        // })
+        // ## Send request card to avoid asyc
+
       }
     })
-    // 获取用户信息
-    wx.getSetting({
+  },
+  getUserInfo: (func, app) => {
+    // console.log("Salmonnnnnnnnnn")
+    let that = app
+    wx.getUserInfo({
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
+        that.globalData.userInfo = res.userInfo
+        console.log("Success get UserInfo")
+        console.log(that.globalData.userInfo)
+        func(res.userInfo)
       }
     })
   },
   globalData: {
     userInfo: null
   }
+  // globalData: {
+  //   userInfo: {
+  //     nickName: "",
+  //     avatarUrl: "",
+  //     gender: "none",
+  //     province: "",
+  //     city: ""
+  //   },
+  //   salmon: "",
+  //   token: "",
+  //   currentUserId: "",
+  //   email: "",
+  //   user: {},
+  //   items: {},
+  //   item: {},
+  //   profileItem: {}
+
+  // }
 })
