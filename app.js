7a7605c63122180f410ff41e530f3fde4ab194c4
Chinzorig <pchinzoo@gmail.com>
2019-04-29 20:51:34 +0800

    will work

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const AV = require('./utils/av-weapp-min.js') 
+const config = require('./key')   
+AV.init({ appId: config.appId, appKey: config.appSecret, });
+
 App({
   onLaunch: function () {
     // WX code
