3e1d2c3820d09346f9263a4b9c108c17fd435ef3
Sevan Golnazarian <sgolnaza@gmail.com>
2019-05-03 22:05:08 +0800

    new spots page cards component design with go there button

---
+++ pages/info-card/info-card.js
@@ -0,0 +1,69 @@
+// pages/info-card/info-card.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    type: 'Ledge',
+    address: '成都市五华区和美东路222号',
+    distance: '3',
+    spotImg: 'https://res.cloudinary.com/doe2rb42f/image/upload/v1556816798/wpl8hemjzd1bjh9jlrdc.jpg'
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
