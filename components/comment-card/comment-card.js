7497424259a740896bd1ce2c6e5cffdf9dcbb325
Sevan Golnazarian <sgolnaza@gmail.com>
2019-05-04 00:57:04 +0800

    comment card for spot page

---
+++ components/comment-card/comment-card.js
@@ -0,0 +1,29 @@
+// components/comment-card/comment-card.js
+Component({
+  /**
+   * Component properties
+   */
+  options: {
+    multipleSlots: true
+  },
+  properties: {
+    avatar: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
