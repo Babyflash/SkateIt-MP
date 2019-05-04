a341c35a180b7c3ac97d3f500a984e1089ac929b
Sevan Golnazarian <sgolnaza@gmail.com>
2019-05-05 02:12:45 +0800

    user page and accelerometer for locaiton

--- pages/sevan/sevan.js
+++ pages/sevan/sevan.js
@@ -72,7 +72,7 @@ Page({
 
   locating: false,
   spotCount: 0,
-
+  
   _hanldeLocation: function () {
     let that = this
     if (that.mapCtx) {
@@ -396,4 +396,25 @@ Page({
     })
     console.log(spotCount)
   },
+  onShow: function () {
+    let that = this
+    this.audioCtx = wx.createAudioContext('myAudio')
+    this.audioCtx.setSrc('http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46')
+    const innerAudioContext = wx.createInnerAudioContext();//新建一个createInnerAudioContext();
+    innerAudioContext.autoplay = true;//音频自动播放设置
+    innerAudioContext.src = '../../lib/audio/notice.mp3';//链接到音频的地址
+    innerAudioContext.onPlay(() => { });//播放音效
+    wx.onAccelerometerChange(function (e) {
+      console.log(e.x)
+      console.log(e.y)
+      console.log(e.z)
+      if (e.x > 1 && e.y > 1) {
+        that._hanldeLocation()
+      }
+    })
+  },
+  onHide: function(){
+    wx.stopAccelerometer()
+  }
+  
 })
